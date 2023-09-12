b48748915975b9852fce4b06b8c6e7bae63b1d52
Peshelo <125571647+Peshelo@users.noreply.github.com>
2023-09-12 10:21:20 +0200

    Initial Commit
    Initial Commit

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules:[
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+    'nuxt-icon',
+    // '@pinia-plugin-persistedstate/nuxt',
+    '@nuxt/image',
+    ['@nuxtjs/google-fonts', {
+      families: {
+        Roboto: true,
+        Inter: [400, 700],
+         'Josefin+Sans': true,
+        Lato: [100, 300],
+        Raleway: {
+          wght: [100, 400],
+          ital: [100]
+        },
+      }
+  }]
+  ],
+  runtimeConfig: {
+    public: {
+      baseURL: process.env.BASE_URL || 'http://localhost:8080/',
+    },
+  },
+})
