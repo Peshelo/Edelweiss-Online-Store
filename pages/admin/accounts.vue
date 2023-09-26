a7fb85c4cc513c30f4c49fdb5b3b9ed9226deafb
unknown <ruramaibotso@gmail.com>
2023-09-26 08:03:47 +0200

    All endpoints except products

--- pages/admin/accounts.vue
+++ pages/admin/accounts.vue
@@ -8,32 +8,33 @@
           <hr class="mt-4 mb-8" />
           <p class="py-2 text-xl font-semibold">Email Address</p>
           <div class="flex flex-col sm:flex-row sm:items-center sm:justify-between">
-            <p class="text-gray-600">Your email address is <strong>john.doe@company.com</strong></p>
-            <button class="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">Change</button>
+            <p class="text-gray-600"><strong>{{this.email}}</strong></p>
+            <!-- <button class="inline-flex text-sm font-semibold text-blue-600 underline decoration-2">Change</button> -->
           </div>
           <hr class="mt-4 mb-8" />
           <p class="py-2 text-xl font-semibold">Password</p>
           <div class="flex items-center">
-            <div class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
+            <form  class="flex flex-col space-y-2 sm:flex-row sm:space-y-0 sm:space-x-3">
               <label for="login-password">
                 <span class="text-sm text-gray-500">Current Password</span>
                 <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                  <input type="password" id="login-password" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                  <input type="password" v-model="password" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                  <p v-if="this.errors.password" class="text-sm text-red-600 text-left mb-2">*{{this.errors.password}}</p>
                 </div>
               </label>
               <label for="login-password">
                 <span class="text-sm text-gray-500">New Password</span>
                 <div class="relative flex overflow-hidden rounded-md border-2 transition focus-within:border-blue-600">
-                  <input type="password" id="login-password" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                  <input type="password" v-model="cpassword" class="w-full flex-shrink appearance-none border-gray-300 bg-white py-2 px-4 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="***********" />
+                  <p v-if="this.errors.cpassword" class="text-sm text-red-600 text-left mb-2">*{{this.errors.cpassword}}</p>
                 </div>
               </label>
-            </div>
+            </form>
             <svg xmlns="http://www.w3.org/2000/svg" class="mt-5 ml-2 h-6 w-6 cursor-pointer text-sm font-semibold text-gray-600 underline decoration-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
               <path stroke-linecap="round" stroke-linejoin="round" d="M13.875 18.825A10.05 10.05 0 0112 19c-4.478 0-8.268-2.943-9.543-7a9.97 9.97 0 011.563-3.029m5.858.908a3 3 0 114.243 4.243M9.878 9.878l4.242 4.242M9.88 9.88l-3.29-3.29m7.532 7.532l3.29 3.29M3 3l3.59 3.59m0 0A9.953 9.953 0 0112 5c4.478 0 8.268 2.943 9.543 7a10.025 10.025 0 01-4.132 5.411m0 0L21 21" />
             </svg>
           </div>
-          <p class="mt-2">Can't remember your current password. <a class="text-sm font-semibold text-blue-600 underline decoration-2" href="#">Recover Account</a></p>
-          <button class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
+          <button @click="updateAdminHandler" class="mt-4 rounded-lg bg-blue-600 px-4 py-2 text-white">Save Password</button>
           <hr class="mt-4 mb-8" />
     
         </div>
@@ -41,15 +42,59 @@
   </template>
   
   <script>
+  import axios from 'axios'
   import adminSettings from "@/layouts/adminSettings.vue";
     export default {
       components: {
       adminSettings
     },
-      
+      data(){
+        return{
+          email: localStorage.email,
+          password: '',
+          cpassword: '',
+          errors:{}
+        }
+      },
+      methods:{
+        async updateAdminHandler(){
+      this.errors = {};
+             
+             if(!this.password){
+                 this.errors.password = "Enter your password";
+             } 
+             if(!(this.password === this.cpassword)){
+                 this.errors.cpassword = "Passwords do not match";
+             }
+             if(!this.cpassword){
+                 this.errors.cpassword = "Re-enter password";
+             }
+           
+            if (Object.keys(this.errors).length === 0) {
+        // make API call or submit form data here
+        try{
+        await axios.put(`http://localhost:8080/auth/updatePassword/${this.email}/${this.password}`,{
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include'
+          }).then((response) =>{
+          const data = response.data;
+          alert("Account updated successfully.")
+          this.response = data;
+          console.log(response);
+        })
+        }catch(err){
+        console.log("Error:",err)
+        this.errors.failed = "Sorry, an error occured!";
+        this.errors.ERR = err;
+        }
+        console.log("Form submitted successfully");
+      }
+    },
+   
+      }
     }
   </script>
   
-  <style lang="scss" scoped>
+  <style>
   
   </style>
