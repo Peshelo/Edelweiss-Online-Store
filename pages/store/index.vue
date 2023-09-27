423068e9de296a810369b1f6d7901383836b713e
Peshelo <125571647+Peshelo@users.noreply.github.com>
2023-09-27 13:00:52 +0200

    Added comment
    Added comment

--- pages/store/index.vue
+++ pages/store/index.vue
@@ -75,6 +75,7 @@
 </template>
 
 <script setup>
+//sd
 import StoreMainLayout from "@/layouts/StoreMainLayout.vue";
 const {data: products,pending,error,refresh} = await useLazyFetch('http://localhost:8080/products?page=0&pageSize=5')
 console.log(products.value)
